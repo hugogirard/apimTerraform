b37b294325ea6ce4f046b6eaee1cffb8ab40af9b
Hugo Girard <64166891+hugogirard@users.noreply.github.com>
2021-04-22 21:10:18 +0000

    added api

--- conference/main.tf
+++ conference/main.tf
@@ -1,19 +1,14 @@
-data "azurerm_resources" "apim" {
-    name                    = var.apimName
-    resource_group_name     = var.resourceGroupName    
+resource "azurerm_api_management_api" "example" {
+  name                = "conference-api"
+  resource_group_name = var.resourceGroupName  
+  api_management_name = var.apimName
+  revision            = "1"
+  display_name        = "Conference API"
+  path                = "example"
+  protocols           = ["https"]
+
+  import {
+    content_format = "swagger-json"
+    content_value  = file("${path.module}/conference-api.json")
+  }
 }
-
-# resource "azurerm_api_management_api" "example" {
-#   name                = "example-api"
-#   resource_group_name = var.resourceGroupName
-#   api_management_name = azurerm_api_management.example.name
-#   revision            = "1"
-#   display_name        = "Example API"
-#   path                = "example"
-#   protocols           = ["https"]
-
-#   import {
-#     content_format = "swagger-json"
-#     content_value  = file("${path.module}/conference-api.json")
-#   }
-# }
